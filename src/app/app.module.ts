3fd1124f6a705f7ab3de5639d7de6349d712f635
ThayrinneLima <thayrinne@prebelli.online>
2022-01-10 22:45:23 -0300

    Criado Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
