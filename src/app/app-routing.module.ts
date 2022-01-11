260d13e71e699e81cd735670ff9f59dce83c5092
ThayrinneLima <thayrinne@prebelli.online>
2022-01-10 23:00:01 -0300

    Criado Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
+import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
+import { DicasComponent } from './pages/dicas/dicas.component';
+import { TrabalhosComponent } from './pages/trabalhos/trabalhos.component';
+import { EstilosComponent } from './pages/estilos/estilos.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"",
+  component:SobreComponent
+},
+
+{
+  path:"/sobre",
+  component:SobreComponent
+},
+{
+  path:"/estilos",
+  component:EstilosComponent
+},
+{
+  path:"/trabalhos",
+  component:TrabalhosComponent
+},
+{
+  path:"/dicas",
+  component:DicasComponent
+},
+{
+  path:"orcamento",
+  component:OrcamentoComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { EstilosComponent } from './pages/estilos/estilos.component';
+import { TrabalhosComponent } from './pages/trabalhos/trabalhos.component';
+import { DicasComponent } from './pages/dicas/dicas.component';
+import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    SobreComponent
+    SobreComponent,
+    EstilosComponent,
+    TrabalhosComponent,
+    DicasComponent,
+    OrcamentoComponent
   ],
   imports: [
     BrowserModule,
